af51f1d91c88b6ffe0abea1929d2e746971b8d82
Johnathanyes <johnathanylee327@gmail.com>
2024-09-25 00:36:03 -0400

    fixed issue with keys

--- app/dashboard/documents/[documentId]/chat-panel.tsx
+++ app/dashboard/documents/[documentId]/chat-panel.tsx
@@ -24,6 +24,7 @@ export default function ChatPanel({
         </div>
         {chats?.map((chat) => (
           <div
+          key={chat._id}
             className={cn(
               {
                 "bg-stone-800": chat.isHuman,
